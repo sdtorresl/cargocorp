d408c8558874d18dd308e6426cb8d78e261eab70
Sergio Torres <sdtorresl@unal.edu.co>
2021-07-20 12:47:54 -0500

    Initial application

---
+++ templates/Pages/home.php
@@ -0,0 +1,404 @@
+<section class="contacto-container">
+    <div class="contacto">
+        <h1 class="title">
+            La industria de seguros con, <strong>todas las linas marine</strong>
+        </h1>
+    </div>
+    <div class="boton">
+        <a href="#" class="boton-contacto">Contáctanos</a>
+    </div>
+
+    <div class="container white-grid pl-3 pr-3">
+        <div class="row justify-content-center top-text d-flex align-content-center flex-wrap">
+            <div class="top-box col-10 col-sm-10 col-md-4 col-lg-4 col-xl-4 pl-0 pr-0 mb-5">
+                <div class="d-flex align-content-center pl-3">
+                    <figure class="align-content-center">
+                        <img src="img/clock.png" alt="#" class=" image-c pr-3 pl-3">
+                    </figure>
+                    <div>
+                        <p class="align-content-center">
+                            Más de 20 años de experiencia en seguros
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="top-box col-10 col-sm-10 col-md-4 col-lg-4 col-xl-4 pl-0 pr-0 mb-5">
+                <div class="d-flex align-content-center">
+                    <figure class="align-content-center">
+                        <img src="img/world.png" alt="#" class=" image-c pr-3 pl-3">
+                    </figure>
+                    <div>
+                        <p class="align-content-center">
+                            Nos encontramos en más de 14 países
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="top-box col-10 col-sm-10 col-md-4 col-lg-4 col-xl-4 pl-0 pr-0 mb-5">
+                <div class="d-flex align-content-center pr-1">
+                    <figure class="align-content-center">
+                        <img src="img/badge.png" alt="#" class=" image-c pr-3 pl-3">
+                    </figure>
+                    <div>
+                        <p class="align-content-center">
+                            Calificacion A-Fitch Rattings, A.M. Best, Standard & Poors, A-
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="container-somos">
+    <div class="lo-que-somos ">
+        <div class="d-flex justify-content-center">
+            <h3 class="trititle d-flex">
+                Lo que somos
+            </h3>
+        </div>
+        <h2 class="subtitle-somos">
+            Una reaseguradora diseñada para ayudarte
+        </h2>
+        <p class="text-cobertura">
+            Tenemos una amplia cobertura que proteje carga en tránsito y almacenamiento. ademas ofrecemos la capacidad de reaseguramiento
+            <span>para sabotaje, terrorismo y riesgos politicos</span>. Asegura tu negocio en las mejores manos
+        </p>
+    </div>
+</section>
+
+<section class="la-diferencia">
+    <div class="d-flex justify-content-center">
+        <h3 class="trititle d-inline-flex">
+            La diferencia
+        </h3>
+    </div>
+    <div class="container p pl-5 pr-5">
+        <div class="row justify-content-center grid-diferencia">
+            <div class="col-12 col-sm-10 col-md-6 col-lg-4 col-xl-4 mb-5 pb-4">
+                <div class="d-flex">
+                    <figure>
+                        <img src="img/engineer.png" alt="#">
+                    </figure>
+                    <div class="text-d">
+                        <p class="title-p">
+                            Profesionales
+                        </p>
+
+                        <p class="text-p">
+                            Tenemos una amplia cobertura que proteje carga en tránsito
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-10 col-md-6 col-lg-4 col-xl-4 mb-5">
+                <div class="d-flex">
+                    <figure>
+                        <img src="img/clipboard.png" alt="#">
+                    </figure>
+                    <div class="text-d">
+                        <p class="title-p">
+                            Profesionales
+                        </p>
+
+                        <p class="text-p">
+                            Tenemos una amplia cobertura que proteje carga en tránsito
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-10 col-md-6 col-lg-4 col-xl-4 mb-5">
+                <div class="d-flex">
+                    <figure>
+                        <img src="img/boxes.png" alt="#">
+                    </figure>
+                    <div class="text-d">
+                        <p class="title-p">
+                            Profesionales
+                        </p>
+                        <p class="text-p">
+                            Tenemos una amplia cobertura que proteje carga en tránsito
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-10 col-md-6 col-lg-4 col-xl-4 mb-5">
+                <div class="d-flex">
+                    <figure>
+                        <img src="img/truck.png" alt="#">
+                    </figure>
+                    <div class="text-d">
+                        <p class="title-p">
+                            Profesionales
+                        </p>
+                        <p class="text-p">
+                            Tenemos una amplia cobertura que proteje carga en tránsito
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-10 col-md-6 col-lg-4 col-xl-4 mb-5">
+                <div class="d-flex">
+                    <figure>
+                        <img src="img/ship-icon.png" alt="#">
+                    </figure>
+                    <div class="text-d">
+                        <p class="title-p">
+                            Profesionales
+                        </p>
+                        <p class="text-p">
+                            Tenemos una amplia cobertura que proteje carga en tránsito
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-10 col-md-6 col-lg-4 col-xl-4 mb-5">
+                <div class="d-flex">
+                    <figure>
+                        <img src="img/shield.png" alt="#">
+                    </figure>
+                    <div class="text-d">
+                        <p class="title-p">
+                            Profesionales
+                        </p>
+                        <p class="text-p">
+                            Tenemos una amplia cobertura que proteje carga en tránsito
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+<section class="nuestros-productos">
+    <div class="title-box d-flex justify-content-center">
+        <h3 class="title-n d-flex">
+            Nuestros productos
+        </h3>
+    </div>
+    <div class="container">
+        <div class="your-class">
+            <div class="d-inline-flex justify-content-center ">
+                <div class="white-background pb-4">
+                    <figure>
+                        <img src="img/ship-image.png" alt="#" class="image-slide">
+                    </figure>
+                    <div class="pl-4 pr-4">
+                        <p class="title-slide">
+                            Transporte general
+                        </p>
+                        <p class="text-slide">
+                            Ofrece un seguro de tránsito que brinda soluciones de seguros innovadores y amplios para satisfacer las necesidades de nuestros asegurados en tránsito internacional.
+                        </p>
+                        <a href="#" class="slide-link">Saber más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="d-inline-flex justify-content-center ">
+                <div class="white-background pb-5">
+                    <figure>
+                        <img src="img/shipload-image.png" alt="#" class="image-slide ">
+                    </figure>
+                    <div class="pl-4 pr-4">
+                        <p class="title-slide">
+                            Stock Throughput
+                        </p>
+                        <p class="text-slide">
+                            Exposiciones más allá del curso normal de tránsito. La cobertura suele estar respaldada por las políticas estándar de tránsito.
+                        </p>
+                        <a href="#" class="slide-link">Saber más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="d-inline-flex justify-content-center ">
+                <div class="white-background pb-5">
+                    <figure>
+                        <img src="img/loaded-ship-image.png" alt="#" class="image-slide">
+                    </figure>
+                    <div class="pl-4 pr-4">
+                        <p class="title-slide">
+                            Carga proyecto
+                        </p>
+                        <p class="text-slide">
+                            Cobertura de equipos destinados a proyectos de infraestructura e instalaciones industriales, La cobertura de la póliza incluye pérdidas físicas.
+                        </p>
+                        <a href="#" class="slide-link">Saber más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="d-inline-flex justify-content-center ">
+                <div class="white-background pb-5">
+                    <figure>
+                        <img src="img/image-slide.png" alt="#" class="image-slide">
+                    </figure>
+                    <div class="pl-4 pr-4">
+                        <p class="title-slide">
+                            Transporte general
+                        </p>
+                        <p class="text-slide">
+                            Ofrece un seguro de tránsito que brinda soluciones de seguros innovadores y amplios para satisfacer las necesidades de nuestros asegurados en tránsito internacional.
+                        </p>
+                        <a href="#" class="slide-link">Saber más</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+
+
+
+<section class="nuestros-mercados">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-1 col-lg-5 col-xl-5">
+            </div>
+            <div class="col-12 col-sm-12 col-md-12 col-lg-7 col-xl-7 left-aling">
+                <div class="d-flex justify-content-center">
+                    <h3 class="trititle title-m">
+                        Nuestros mercados
+                    </h3>
+                </div>
+            </div>
+        </div>
+        <div class="contrys-container ">
+            <div class="row">
+                <div class="col-10 col-sm-10 col-md-10 col-lg-5 col-xl-5">
+                    <div>
+                        <figure>
+                            <img src="img/map-image.png" alt="" class="mapa">
+                        </figure>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-12 col-lg-7 col-xl-7">
+                    <div class="row pt-5 pl-4 big-scale">
+                        <div class="col-10 col-sm-10 col-md-4 col-lg-4 col-xl-4 ">
+                            <div class="country-list">
+                                <h2 class="region">
+
+                                    América del sur
+                                </h2>
+                                <ul>
+                                    <li class="country">Argentina</li>
+                                    <li class="country">Bolivia</li>
+                                    <li class="country">Chile</li>
+                                    <li class="country">Colombia</li>
+                                    <li class="country">Ecuador</li>
+                                    <li class="country">Paraguay</li>
+                                    <li class="country">Perú</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-10 col-sm-10 col-md-4 col-lg-4 col-xl-4">
+                            <div class="country-list">
+                                <h2 class="region">
+                                    América central
+                                </h2>
+                                <ul>
+                                    <li class="country">Guatemala</li>
+                                    <li class="country">Honduras</li>
+                                    <li class="country">Panamá</li>
+                                    <li class="country">El Salvador</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-10 col-sm-10 col-md-4 col-lg-4 col-xl-4">
+                            <div class="country-list">
+                                <h2 class="region">
+                                    Caribe
+                                </h2>
+                                <ul>
+                                    <li class="country">Puerto Rico</li>
+                                    <li class="country">República Dominicana</li>
+                                    <li class="mexico">México</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="noticias">
+    <div class="d-flex justify-content-center ">
+        <h3 class="trititle d-inline-flex">
+            Noticias de interés
+        </h3>
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-11 col-sm-11 col-md-4 col-lg-4 col-xl-4 d-flex mb-3">
+                <div class=" container-news grey-background">
+                    <figure>
+                        <img class="w-100" src="img/containers-image.png" alt="">
+                    </figure>
+                    <div class="news-text">
+                        <p class="noticias-fecha pt-3">
+                            16 de junio 2021
+                        </p>
+                        <h5 class="noticias-title">
+                            Perdida de contenedores en el mar se ha incrementado este año
+                        </h5>
+                        <p class="noticias-text">
+                            En el primer Q del 2021 se han perdido 1.127 contenedores de transporte marítimo, estimando una perdida en mercancía equivalente a unos 54 millones de dólares aproximadamente.
+                        </p>
+                        <a href="#" class="noticias-link">Leer más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-11 col-sm-11 col-md-8 col-lg-8 col-xl-8">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-3 mt-3">
+                        <div class="d-flex container-news grey-background block-d">
+                            <figure class="align-content-center">
+                                <img class="image-news" src="img/plants-image.png" alt="">
+                            </figure>
+                            <div class="news-text pt-3">
+                                <p class="noticias-fecha ">
+                                    16 de junio 2021
+                                </p>
+                                <h5 class=" noticias-title">
+                                    Perdida de contenedores en el mar se ha incrementado este año
+                                </h5>
+                                <p class="noticias-text">
+                                    En el primer Q del 2021 se han perdido 1.127 contenedores de transporte marítimo, estimando una perdida en mercancía equivalente a unos 54 millones de dólares aproximadamente.
+                                </p>
+                                <a href="#" class="noticias-link">Leer más</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mt-3">
+                        <div class="d-flex container-news grey-background block-d">
+                            <figure class="align-content-center">
+                                <img class="image-news" src="img/twist-containers-image.png" alt="">
+                            </figure>
+                            <div class="news-text pt-3">
+                                <p class="noticias-fecha">
+                                    16 de junio 2021
+                                </p>
+                                <h5 class="noticias-title">
+                                    Perdida de contenedores en el mar se ha incrementado este año
+                                </h5>
+                                <p class="noticias-text">
+                                    En el primer Q del 2021 se han perdido 1.127 contenedores de transporte marítimo, estimando una perdida en mercancía equivalente a unos 54 millones de dólares aproximadamente.
+                                </p>
+                                <a href="#" class="noticias-link">Leer más</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="buttom-noticias">
+        <a href="#" class="bottom-button">Ver todas las noticias</a>
+    </div>
+</section>
---
+++ templates/layout/default.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         0.10.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ * @var \App\View\AppView $this
+ */
+
+$cakeDescription = 'CargoCorp UW es una reaseguradora....';
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?= $this->Html->charset() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        <?= $cakeDescription ?>:
+        <?= $this->fetch('title') ?>
+    </title>
+    <?= $this->Html->meta('icon') ?>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700;800&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <?= $this->Html->css('normalize') ?>
+    <?= $this->Html->css('/vendor/slick/slick.css') ?>
+    <?= $this->Html->css('/vendor/slick/slick-theme.css') ?>
+    <?= $this->Html->css('style') ?>
+
+    <?= $this->fetch('meta') ?>
+    <?= $this->fetch('css') ?>
+    <?= $this->fetch('script') ?>
+</head>
+
+<body>
+    <header>
+        <div class="menu-container">
+            <nav class="navbar navbar-expand-lg navbar-dark p-0">
+                <a class="navbar-brand" href="#">
+                    <figure>
+                        <img src="img/Group.png" alt="#" class="logo">
+                    </figure>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto menu mb-3 mt-3">
+                        <li class="nav-item mb-3">
+                            <a class="white-leter" href="#">Inicio<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item mb-3">
+                            <a class="white-leter " href="#">Noticias</a>
+                        </li>
+                        <li class="nav-item mb-3">
+                            <a class="white-leter" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Sobre nosotros
+                            </a>
+                        </li>
+                        <li class="nav-item mb-3">
+                            <a class="white-leter" href="#">Inicio sesión</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <main class="main">
+        <?= $this->Flash->render() ?>
+        <?= $this->fetch('content') ?>
+    </main>
+
+    <footer>
+        <p class="Copyright">
+            Copyright © 2012 CargoCorp Uderwriters | All Right Reserved
+        </p>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <?= $this->Html->script('/vendor/slick/slick.min.js') ?>
+    <?= $this->Html->script('main') ?>
+    </script>
+</body>
+
+</html>
