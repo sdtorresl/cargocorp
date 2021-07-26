a813fcfde33fdbd8f1207a111464c0ac08f154df
Sergio Torres <sdtorresl@unal.edu.co>
2021-07-25 20:23:18 -0500

    Refactor

--- templates/Pages/products.php
+++ templates/Pages/products.php
@@ -1,116 +1,116 @@
-<section>
-	<figure>
-		<?= $this->Html->image('ship-background.png', ['alt' => 'ship-image-background']); ?>
-	</figure>
-</section>
+<aside class="product-picture">
+    <figure>
+        <?= $this->Html->image('ship-background.png', ['alt' => 'ship-image-background']); ?>
+    </figure>
+</aside>
 
 <section class="about-product">
-	<div class="container-products">
-		<div class="d-flex justify-content-center content-left">
-			<h3 class="trititle d-inline-flex " id="transform-title">
-				sobre el producto
-			</h3>
-		</div>
-	
-		<h2 id="about-title">
-			Transporte general
-		</h2>
-		<p id="about-text">
-			Ofrece un seguro de tránsito que brinda soluciones de seguros innovadores y amplios para satisfacer las necesidades de nuestros asegurados en tránsito internacional. Los límites de hasta $ 15M USD en capacidad están disponibles, sujetos a las directrices corporativas; Sin embargo, la capacidad adicional está disponible si es necesario. Además del riesgo de tránsito, podemos extender nuestra política para incluir riesgos de almacenamiento para satisfacer las necesidades de nuestros asegurados
-		</p>
-	</div>
+    <div class="container-products">
+        <div class="d-flex justify-content-center content-left">
+            <h3 class="trititle d-inline-flex " id="transform-title">
+                sobre el producto
+            </h3>
+        </div>
+
+        <h2 id="about-title">
+            Transporte general
+        </h2>
+        <p id="about-text">
+            Ofrece un seguro de tránsito que brinda soluciones de seguros innovadores y amplios para satisfacer las necesidades de nuestros asegurados en tránsito internacional. Los límites de hasta $ 15M USD en capacidad están disponibles, sujetos a las directrices corporativas; Sin embargo, la capacidad adicional está disponible si es necesario. Además del riesgo de tránsito, podemos extender nuestra política para incluir riesgos de almacenamiento para satisfacer las necesidades de nuestros asegurados
+        </p>
+    </div>
 </section>
 
-<section class="recognition">	
-	<div class="container-products">
-		<div>
-			<h2 class="recognition-title">
-				Reconocimiento especializado de linea a través de Latinoamérica		
-			</h2>
-		</div>
-		<div>
-			<ul>
-				<li class="recognition-list-item">Generadores de carga</li>
-				<li class="recognition-list-item">Grandes superficies</li>
-				<li class="recognition-list-item">Compañías de industria y comercio</li>
-			</ul>
-		</div>
-	</div>
+<section class="recognition">
+    <div class="container-products">
+        <div>
+            <h2 class="recognition-title">
+                Reconocimiento especializado de linea a través de Latinoamérica
+            </h2>
+        </div>
+        <div>
+            <ul>
+                <li class="recognition-list-item">Generadores de carga</li>
+                <li class="recognition-list-item">Grandes superficies</li>
+                <li class="recognition-list-item">Compañías de industria y comercio</li>
+            </ul>
+        </div>
+    </div>
 </section>
 
 <section class="nuestros-productos">
-	<div class="title-box d-flex justify-content-center">
-		<h3 class="title-n d-flex">
-			Nuestros productos
-		</h3>
-	</div>
-	<div class="container">
-		<div class="products-slider">
-			<div class="d-inline-flex justify-content-center ">
-				<div class="white-background pb-4">
-					<figure>
-						<?= $this->Html->image('ship-image.png', ['alt' => 'ship-image', 'class'=>'image-slide']); ?>
-					</figure>
-					<div class="pl-4 pr-4">
-						<p class="title-slide">
-							Transporte general
-						</p>
-						<p class="text-slide">
-							Ofrece un seguro de tránsito que brinda soluciones de seguros innovadores y amplios para satisfacer las necesidades de nuestros asegurados en tránsito internacional. 
-						</p>
-						<a href="#" class="slide-link">Saber más</a>
-					</div>	
-				</div>
-			</div>
-			<div class="d-inline-flex justify-content-center ">
-				<div class="white-background pb-5">
-					<figure>
-						<?= $this->Html->image('shipload-image.png', ['alt' => 'shipload-image', 'class'=>'image-slide']); ?>
-					</figure>
-					<div class="pl-4 pr-4">
-						<p class="title-slide">
-							Stock Throughput
-						</p>
-						<p class="text-slide">
-							Exposiciones más allá del curso normal de tránsito. La cobertura suele estar respaldada por las políticas estándar de tránsito.
-						</p>
-						<a href="#" class="slide-link">Saber más</a>
-					</div>
-				</div>
-			</div>
-			<div class="d-inline-flex justify-content-center ">
-				<div class="white-background pb-5">
-					<figure>
-						<?= $this->Html->image('loaded-ship-image.png', ['alt' => 'loaded-ship-image', 'class'=>'image-slide']); ?>
-					</figure>
-					<div class="pl-4 pr-4">
-						<p class="title-slide">
-							Carga proyecto
-						</p>
-						<p class="text-slide">
-							Cobertura de equipos destinados a proyectos de infraestructura e instalaciones industriales, La cobertura de la póliza incluye pérdidas físicas.
-						</p>
-						<a href="#" class="slide-link">Saber más</a>
-					</div>	
-				</div>
-			</div>
-			<div class="d-inline-flex justify-content-center ">
-				<div class="white-background pb-5">
-						<figure>
-							<img src="img/image-slide.png" alt="#" class="image-slide">
-						</figure>
-						<div class="pl-4 pr-4">
-							<p class="title-slide">
-								Transporte general
-							</p>
-							<p class="text-slide">
-								Ofrece un seguro de tránsito que brinda soluciones de seguros innovadores y amplios para satisfacer las necesidades de nuestros asegurados en tránsito internacional. 
-							</p>
-							<a href="#" class="slide-link">Saber más</a>
-						</div>
-					</div>	
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="title-box d-flex justify-content-center">
+        <h3 class="title-n d-flex">
+            Nuestros productos
+        </h3>
+    </div>
+    <div class="container">
+        <div class="products-slider">
+            <div class="d-inline-flex justify-content-center ">
+                <div class="white-background pb-4">
+                    <figure>
+                        <?= $this->Html->image('ship-image.png', ['alt' => 'ship-image', 'class' => 'image-slide']); ?>
+                    </figure>
+                    <div class="pl-4 pr-4">
+                        <p class="title-slide">
+                            Transporte general
+                        </p>
+                        <p class="text-slide">
+                            Ofrece un seguro de tránsito que brinda soluciones de seguros innovadores y amplios para satisfacer las necesidades de nuestros asegurados en tránsito internacional.
+                        </p>
+                        <a href="#" class="slide-link">Saber más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="d-inline-flex justify-content-center ">
+                <div class="white-background pb-5">
+                    <figure>
+                        <?= $this->Html->image('shipload-image.png', ['alt' => 'shipload-image', 'class' => 'image-slide']); ?>
+                    </figure>
+                    <div class="pl-4 pr-4">
+                        <p class="title-slide">
+                            Stock Throughput
+                        </p>
+                        <p class="text-slide">
+                            Exposiciones más allá del curso normal de tránsito. La cobertura suele estar respaldada por las políticas estándar de tránsito.
+                        </p>
+                        <a href="#" class="slide-link">Saber más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="d-inline-flex justify-content-center ">
+                <div class="white-background pb-5">
+                    <figure>
+                        <?= $this->Html->image('loaded-ship-image.png', ['alt' => 'loaded-ship-image', 'class' => 'image-slide']); ?>
+                    </figure>
+                    <div class="pl-4 pr-4">
+                        <p class="title-slide">
+                            Carga proyecto
+                        </p>
+                        <p class="text-slide">
+                            Cobertura de equipos destinados a proyectos de infraestructura e instalaciones industriales, La cobertura de la póliza incluye pérdidas físicas.
+                        </p>
+                        <a href="#" class="slide-link">Saber más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="d-inline-flex justify-content-center ">
+                <div class="white-background pb-5">
+                    <figure>
+                        <img src="img/image-slide.png" alt="#" class="image-slide">
+                    </figure>
+                    <div class="pl-4 pr-4">
+                        <p class="title-slide">
+                            Transporte general
+                        </p>
+                        <p class="text-slide">
+                            Ofrece un seguro de tránsito que brinda soluciones de seguros innovadores y amplios para satisfacer las necesidades de nuestros asegurados en tránsito internacional.
+                        </p>
+                        <a href="#" class="slide-link">Saber más</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
 </section>
